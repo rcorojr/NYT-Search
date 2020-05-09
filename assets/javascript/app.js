3e8e1aa82c58446cc33ce11c288d69efd16ee246
MatthewBub <6matbub@gmail.com>
2020-05-09 13:01:43 -0700

    handleQuery init

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,18 @@
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=zmW5EKTTjmqn8ZaBEF791w7YD1RurxKA";
 
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+  
+    return queryItems.join('&');
+}
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+.then(function(response){
+  console.log(response)
+});
