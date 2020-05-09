9b071afef0a006ffddbe3d2944c7df8f018db727
Mark F <the.mark.fullton@gmail.com>
2020-05-09 14:05:37 -0700

    Changed JQuery AJAX to fetch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,18 @@
 
-$("#find-article").on("click", function(event){
+$("form").on("submit", function(event){
   event.preventDefault();
 
-  var searchTerm = $(this).val().trim();
+  var searchTerm = $("#article-search").val().trim();
 
   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ searchTerm +"&api-key=zmW5EKTTjmqn8ZaBEF791w7YD1RurxKA";
 
-  $.ajax({
-    url: queryURL,
+  fetch(queryURL, {
+    //url: queryURL,
     method: "GET"
   }).then(function(response){
 
     console.log(response);
 
-    $("#article-view").text(JSON.stringify(response));
+    $("#articles-section").text(JSON.stringify(response));
   })
 })
