6d62d565239b62f7b37cc5935be3f02215cd0c6d
Mark F <the.mark.fullton@gmail.com>
2020-05-09 13:51:32 -0700

    Added searchTerm to QueryURL on js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,11 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=zmW5EKTTjmqn8ZaBEF791w7YD1RurxKA";
 
-$("#find-article").on("click", handleQuery = (e) => {
+$("#find-article").on("click", function(event){
   event.preventDefault();
+
+  var searchTerm = $(this).val().trim();
+
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ searchTerm +"&api-key=zmW5EKTTjmqn8ZaBEF791w7YD1RurxKA";
+
   $.ajax({
     url: queryURL,
     method: "GET"
